cd932118c8ede5691c28f14aef54faf83d12a235
ProllyGG <kortlever.jalen@gmail.com>
2025-07-26 20:25:18 -0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
-// Live XRP price ticker
-async function fetchXRP() {
-  try {
-    const res = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=xrp&vs_currencies=usd');
-    const data = await res.json();
-    console.log('XRP Price:', data.xrp.usd);
-    // You could insert it into the DOM here if you like
-  } catch(e) {
-    console.error(e);
-  }
-}
-fetchXRP();
-setInterval(fetchXRP, 30000);
+VANTA.WAVES({
+  el: "#vanta-bg",
+  mouseControls: true,
+  touchControls: true,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  color: 0x00ffff,
+  shininess: 80.0,
+  waveHeight: 20.0,
+  waveSpeed: 0.6,
+  zoom: 0.85
+});
