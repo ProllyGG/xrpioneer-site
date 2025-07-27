313020c49f9a9c7b0c694cc57ec9c0fa2d90f589
ProllyGG <kortlever.jalen@gmail.com>
2025-07-26 20:27:15 -0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,14 +1,1 @@
-VANTA.WAVES({
-  el: "#vanta-bg",
-  mouseControls: true,
-  touchControls: true,
-  minHeight: 200.00,
-  minWidth: 200.00,
-  scale: 1.0,
-  scaleMobile: 1.0,
-  color: 0x00ffff,
-  shininess: 80.0,
-  waveHeight: 20.0,
-  waveSpeed: 0.6,
-  zoom: 0.85
-});
+console.log("XRPioneers V1 loaded.");
